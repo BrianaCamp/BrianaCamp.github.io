a81631ef652d89af8ff61b51bc5b2def394042c8
Briana Camp <brianarcamp@gmail.com>
2015-06-22 22:44:45 -0700

    add smooth-scroll to navigation

--- js/script.js
+++ js/script.js
@@ -197,4 +197,20 @@ $(window).on('scroll', function() {
   $('#handrawn-linev4:in-viewport').lazylinepainter('paint');
 });
 
+//from paululund.co.uk
+
+$('a[href^="#"]').on('click',function (e) {
+	  e.preventDefault();
+
+	  var target = this.hash;
+	  var $target = $(target);
+
+	  $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
 });
